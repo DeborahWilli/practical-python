c9629bc8629f6211615741aa19047a670005cd10
Deborah Walz <deborah.walz@gmx.de>
2020-11-20 15:44:01 +0100

    neu

--- Work/report.py
+++ Work/report.py
@@ -7,13 +7,24 @@ import sys
 from pprint import pprint
 
 def read_portfolio(filename):
+    '''
+    Read a stock portfolio file into a list of dictionaries with keys
+    name, shares, and price.
+    '''
     portfolio = []
-    with open(filename, "rt") as f:
+    with open(filename) as f:
         rows = csv.reader(f)
         headers = next(rows)
+
         for row in rows:
-            stock = {"name": row[0], "shares": int(row[1]), "price": float(row[2])}
+            record = dict(zip(headers, row))
+            stock = {
+                 'name': record['name'],
+                 'shares': int(record['shares']),
+                 'price': float(record['price'])
+            }
             portfolio.append(stock)
+
     return portfolio
 
 def read_prices(filename):
@@ -28,23 +39,23 @@ def read_prices(filename):
                 prices[row[0]] = float(row[1])
             except IndexError:
                 pass
+
     return prices
 
+
 def make_report(portfolio, prices):
+    '''
+    Make a list of (name, shares, price, change) tuples given a portfolio list
+    and prices dictionary.
+    '''
     report = []
     for pf in portfolio:
-        stock = (pf["name"], pf["shares"], prices[pf["name"]], (prices[pf["name"]]-pf["price"]))
-        report.append(stock)
+        current_price = prices[pf["name"]]
+        change = current_price - pf["price"]
+        summary = (pf["name"], pf["shares"], current_price, change)
+        report.append(summary)
     return report
 
-# for r in report:
-#         print(r)
-
-# ('AA', 100, 9.22, -22.980000000000004)
-# ('IBM', 50, 106.28, 15.180000000000007)
-# ('CAT', 150, 35.46, -47.98)
-# ('MSFT', 200, 20.89, -30.339999999999996)
-# ('GE', 95, 13.48, -26.889999999999997)
 
 # Calling the functions
 portfolio = read_portfolio("Data/portfolio.csv")
@@ -53,15 +64,15 @@ report = make_report(portfolio, prices)
 
 
 # Calculate the total cost of the portfolio
-total_cost = 0.0
-for s in portfolio:
-    total_cost += s["shares"] * s["price"]
+#total_cost = 0.0
+#for s in portfolio:
+#    total_cost += s["shares"] * s["price"]
 #print("Total cost:", total_cost)
 
 # Compute the current value of the portfolio
-total_value = 0.0
-for s in portfolio:
-    total_value += s["shares"] * prices[s["name"]]
+#total_value = 0.0
+#for s in portfolio:
+#    total_value += s["shares"] * prices[s["name"]]
 #print("Current value:", total_value)
 #print("Gain:", total_value - total_cost)
 
